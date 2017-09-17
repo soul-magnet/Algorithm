05d5b90ce9b025bc1820945086cae791d3e1b040
soul-magnet <topkaya.emineirmak@gmail.com>
2017-09-16 21:02:06 -0400

    Update InorderSuccessorInBST.java

--- src/main/java/Ladders/binarytree/InorderSuccessorInBST.java
+++ src/main/java/Ladders/binarytree/InorderSuccessorInBST.java
@@ -51,4 +51,31 @@ public class InorderSuccessorInBST {
 	        	root = root.left;
 	        return root;
 	    }
+	
+	
+	//recursive solution
+	public TreeNode successor(TreeNode root, TreeNode p) {
+	  if (root == null)
+	    return null;
+
+	  if (root.val <= p.val) {
+	    return successor(root.right, p);
+	  } else {
+	    TreeNode left = successor(root.left, p);
+	    return (left != null) ? left : root;
+	  }
+	}
+	//Predecessor - recursive solutuion
+
+	public TreeNode predecessor(TreeNode root, TreeNode p) {
+	  if (root == null)
+	    return null;
+
+	  if (root.val >= p.val) {
+	    return predecessor(root.left, p);
+	  } else {
+	    TreeNode right = predecessor(root.right, p);
+	    return (right != null) ? right : root;
+	  }
+	}
 }
