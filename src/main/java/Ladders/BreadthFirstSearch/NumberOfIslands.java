59e7362540b556938e4aa8b146c14a4ac07ebc67
soul-magnet <topkaya.emineirmak@gmail.com>
2017-10-09 14:31:34 -0400

    adding description

--- src/main/java/Ladders/BreadthFirstSearch/NumberOfIslands.java
+++ src/main/java/Ladders/BreadthFirstSearch/NumberOfIslands.java
@@ -1,5 +1,24 @@
 package BreadthFirstSearch;
 
+/* Given a boolean 2D matrix, 0 is represented as the sea, 
+ * 1 is represented as the island. If two 1 is adjacent, 
+ * we consider them in the same island. We only consider up/down/left/right adjacent.
+ * Find the number of islands.
+
+Example
+Given graph:
+
+[
+  [1, 1, 0, 0, 0],
+  [0, 1, 0, 0, 1],
+  [0, 0, 0, 1, 1],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1]
+]
+return 3.
+
+Tags: Facebook Google Zenefits*/
+
 public class NumberOfIslands {
 
 }
