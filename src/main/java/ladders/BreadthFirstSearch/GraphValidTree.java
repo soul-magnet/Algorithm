b94033e6c0834564ccfe5b58b718e136c9029462
soul-magnet <topkaya.emineirmak@gmail.com>
2018-02-12 17:51:28 -0500

    Graph Valid Tree BFS Approach

--- src/main/java/ladders/BreadthFirstSearch/GraphValidTree.java
+++ src/main/java/ladders/BreadthFirstSearch/GraphValidTree.java
@@ -1,4 +1,10 @@
 package main.java.ladders.BreadthFirstSearch;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 178. Graph Valid Tree - Medium
 
@@ -22,5 +28,53 @@ Medium Connected Component in Undirected Graph 25 %
  * */
 
 public class GraphValidTree {
+	 /**
+     * @param n an integer
+     * @param edges a list of undirected edges
+     * @return true if it's a valid tree, or false
+     */
+     //BFS Approach - Its like course schedule - topological sort
+	/*
+	 * Thoughts: Typical BFS, 
+	 * 1st generate adjacency list by adding edges to each other
+	 * 2nd keep Queue and HashSet 
+	 * 3rd traverse in the queue also traverse by the size of adjacencyList
+	 * 4th if the edge has not been visited in adjacencyList add to the queue
+	 * 5th return if the size of hashSet equal to the size of given n
+	 * */
+     	public boolean validTree1(int n, int[][] edges) {
+     		if(n == 0 || !(edges.length == n-1)) return false;
+     		
+     		//generate the graph
+     		ArrayList<Integer>[] adjacencyList = new ArrayList[n];
+     		for(int i = 0; i < n; i++) {
+     			adjacencyList[i] = new ArrayList<Integer>();
+     		}
+     		//undirected edges
+     		for(int i = 0; i < edges.length; i++) {
+     			adjacencyList[edges[i][0]].add(edges[i][1]);
+     			adjacencyList[edges[i][1]].add(edges[i][0]);
+     		}
+     		
+     		Queue<Integer>queue = new LinkedList<Integer>();
+     		queue.add(0);
+     		//no duplicate edges will appear in the edges
+     		HashSet<Integer> hashSet = new HashSet<Integer>();
+     		hashSet.add(0);
+     		
+     		while(!queue.isEmpty()) {
+     			int size = queue.size();
+     			for(int i = 0; i < size; i++) {
+     				int node = queue.poll();
+     				hashSet.add(node);
+     				for(int j = 0; j < adjacencyList[node].size(); j++) {
+     					if(!hashSet.contains(adjacencyList[node].get(j))) {
+     						queue.offer(adjacencyList[node].get(j));
+     					}
+     				}
+     			}
+     		}
+     		return hashSet.size() == n; //to be able to tree there shouldn't be cycle
+     	}
 
 }
