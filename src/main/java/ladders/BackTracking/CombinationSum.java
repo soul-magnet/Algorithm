9103817eeae239e981869255addba6a32afca7c2
soul-magnet <topkaya.emineirmak@gmail.com>
2018-03-05 09:23:19 -0500

    Backtrack TODO

---
+++ src/main/java/ladders/BackTracking/CombinationSum.java
@@ -0,0 +1,31 @@
+package main.java.ladders.BackTracking;
+/**
+ * 135. Combination Sum - Medium - Required
+
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+ Notice
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The solution set must not contain duplicate combinations.
+
+Example
+Given candidate set [2,3,6,7] and target 7, a solution set is:
+
+[7]
+[2, 2, 3]
+
+Tags: Array, Backtracking
+
+Related Problems 
+Medium Coin Change II 28 %
+Hard Compute 24 Game 31 %
+Hard Add Operators 27 %
+Medium Factorization 28 %
+Medium Combination Sum II 30 %
+ * */
+public class CombinationSum {
+
+}
