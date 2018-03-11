d205b0968c0e527d88bce0cd59181da7fef74863
soul-magnet <topkaya.emineirmak@gmail.com>
2018-03-11 18:49:28 -0400

    Trie TODOs

---
+++ src/main/java/ladders/Trie/MaximumSubArray6.java
@@ -0,0 +1,38 @@
+package main.java.ladders.Trie;
+/**
+ * 722. Maximum Subarray VI - Hard
+
+Given an array of integers. find the maximum XOR subarray value in given array.
+
+What's the XOR: https://en.wikipedia.org/wiki/Exclusive_or
+
+ Notice
+Expected time complexity O(n).
+
+Example
+Given nums = [1, 2, 3, 4], return 7
+The subarray [3, 4] has maximum XOR value
+
+Given nums = [8, 1, 2, 12, 7, 6], return 15
+The subarray [1, 2, 12] has maximum XOR value
+
+Given nums = [4, 6], return 6
+The subarray [6] has maximum XOR value
+
+Tags 
+Trie Bit Manipulation Facebook
+Related Problems 
+Hard Next Sparse Number 25 %
+Hard Maximum Subarray V 30 %
+Medium Maximum Subarray IV 35 %
+ * */
+public class MaximumSubArray6 {
+	 /**
+     * @param nums: the array
+     * @return: the max xor sum of the subarray in a given array
+     */
+    public int maxXorSubarray(int[] nums) {
+        // write code here
+    }
+
+}
---
+++ src/main/java/ladders/Trie/PalindromePairs.java
@@ -0,0 +1,37 @@
+package main.java.ladders.Trie;
+
+import java.util.List;
+
+/**
+ * 775. Palindrome Pairs - Hard
+
+Given a list of unique words, find all pairs of distinct indices (i, j) in the given list, 
+so that the concatenation of the two words, i.e. words[i] + words[j] is a palindrome.
+
+Have you met this question in a real interview? 
+Example
+Given words = ["bat", "tab", "cat"]
+Return [[0, 1], [1, 0]]
+The palindromes are ["battab", "tabbat"]
+
+Given words = ["abcd", "dcba", "lls", "s", "sssll"]
+Return [[0, 1], [1, 0], [3, 2], [2, 4]]
+The palindromes are ["dcbaabcd", "abcddcba", "slls", "llssssll"]
+
+Tags: Hash Table Trie String Airbnb Google
+
+Related Problems 
+Easy Palindrome Number II 46 %
+Medium Longest Palindromic Subsequence 43 %
+Medium Longest Palindromic Substring 29 %
+ * */
+public class PalindromePairs {
+	/**
+     * @param words: a list of unique words
+     * @return: all pairs of distinct indices
+     */
+    public List<List<Integer>> palindromePairs(String[] words) {
+        // Write your code here
+    }
+
+}
