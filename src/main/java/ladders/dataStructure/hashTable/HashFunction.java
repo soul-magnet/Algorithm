28b740183598a67caf9cabdf9a3c1e5900d941a5
soul-magnet <topkaya.emineirmak@gmail.com>
2018-03-12 14:31:29 -0400

    commit to the master

--- src/main/java/ladders/dataStructure/hashTable/HashFunction.java
+++ src/main/java/ladders/dataStructure/hashTable/HashFunction.java
@@ -1,10 +1,11 @@
 package dataStructure.hashTable;
-/*
- * 
- * In data structure Hash, hash function is used to convert a string(or any other type) into an integer smaller than hash size and bigger or equal to zero.
- * The objective of designing a hash function is to "hash" the key as unreasonable as possible.
- * A good hash function can avoid collision as less as possible. 
- * A widely used hash function algorithm is using a magic number 33, consider any string as a 33 based big integer like follow:
+/**
+128. Hash Function 
+ 
+In data structure Hash, hash function is used to convert a string(or any other type) into an integer smaller than 
+hash size and bigger or equal to zero. The objective of designing a hash function is to "hash" the key as unreasonable as possible. 
+A good hash function can avoid collision as less as possible. A widely used hash function algorithm is using a magic number 33, 
+consider any string as a 33 based big integer like follow:
 
 hashcode("abcd") = (ascii(a) * 333 + ascii(b) * 332 + ascii(c) *33 + ascii(d)) % HASH_SIZE 
 
@@ -17,16 +18,18 @@ here HASH_SIZE is the capacity of the hash table (you can assume a hash table is
 Given a string as a key and the size of hash table, return the hash value of this key.f
 
 
+Clarification
+For this problem, you are not necessary to design your own hash algorithm or consider any collision issue, 
+you just need to implement the algorithm as described.
 
-Have you met this question in a real interview? Yes
 Example
 For key="abcd" and size=100, return 78
 
-Clarification
-For this problem, you are not necessary to design your own hash algorithm or consider any collision issue, you just need to implement the algorithm as described.*/
+Tags: Hash Table
+*/
 public class HashFunction {
 
-	/**
+   /**
      * @param key: A String you should hash
      * @param HASH_SIZE: An integer
      * @return an integer
