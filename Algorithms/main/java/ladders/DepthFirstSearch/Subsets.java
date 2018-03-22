141e69bfe4eca51062d1b46dee950e56a39b8baa
soul-magnet <topkaya.emineirmak@gmail.com>
2018-03-22 10:12:15 -0400

    subsets combination DFS

--- Algorithms/main/java/ladders/DepthFirstSearch/Subsets.java
+++ Algorithms/main/java/ladders/DepthFirstSearch/Subsets.java
@@ -45,20 +45,25 @@ public class Subsets {
      * @return: A list of lists. All valid subsets.
      */
      
-     public List<List<Integer>> subsets(int[] nums) {
+     public static List<List<Integer>> subsets(int[] nums) {
     	 List<List<Integer>>result = new ArrayList<>();
     	 Arrays.sort(nums);
     	 backtrack(result, new ArrayList<Integer>(), nums, 0);
     	 return result;
      }
      
-     private void backtrack(List<List<Integer>>result, List<Integer>tempList, int[]nums, int pos) {
+     private static void backtrack(List<List<Integer>>result, List<Integer>tempList, int[]nums, int pos) {
     	 result.add(new ArrayList<>(tempList));
     	 for(int i = pos; i < nums.length; i++) {
     		 tempList.add(nums[i]);
     		 backtrack(result, tempList, nums, i+1);
     		 tempList.remove(tempList.size() - 1);
     	 }
+     }
+     
+     public static void main(String[] args) {
+    	 int nums[] = {1, 2, 3};
+    	 System.out.println(subsets(nums));
      }
 
 }
