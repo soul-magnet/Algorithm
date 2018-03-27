172ca3c8f46679acc5c0041fee08ced3256fb15b
soul-magnet <topkaya.emineirmak@gmail.com>
2018-03-27 09:59:12 -0400

    permutation2 dfs based

--- Algorithms/main/java/ladders/DepthFirstSearch/Permutations2.java
+++ Algorithms/main/java/ladders/DepthFirstSearch/Permutations2.java
@@ -8,16 +8,15 @@ import java.util.Collections;
 
 Given a list of numbers with duplicate number in it. Find all unique permutations.
 
-Example
-For numbers [1,2,2] the unique permutations are:
+Example: For numbers [1,2,2] the unique permutations are:
 
 [
   [1,2,2],
   [2,1,2],
   [2,2,1]
 ]
-Challenge 
-Using recursion to do it is acceptable. If you can do it without recursion, that would be great!
+
+Challenge: Using recursion to do it is acceptable. If you can do it without recursion, that would be great!
 
 Tags 
 Recursion LinkedIn Depth First Search
