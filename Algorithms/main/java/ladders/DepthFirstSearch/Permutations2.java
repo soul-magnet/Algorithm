647bdc2885179ac11b4008e14df9fd76bcbff3b8
soul-magnet <topkaya.emineirmak@gmail.com>
2018-04-02 22:15:21 -0400

    Permutation Based DFS - Backtracking

--- Algorithms/main/java/ladders/DepthFirstSearch/Permutations2.java
+++ Algorithms/main/java/ladders/DepthFirstSearch/Permutations2.java
@@ -30,17 +30,29 @@ Medium Permutations 28 %
  * 
  * */
 
-//
+//TODO: USING SET IMPLEMENTATION
+
 /* Analysis: DFS -Backtracking - Recursion Tree
  * What do I need?
  * 1. Result List
  * 2. tempList, keeps the current permutation until the count of each index equal 0
- * 3. visited boolean value
- * 
+ * 3. visited boolean value - to skip duplicates we have to keep track visited indexes
+ * -----------------------------------------------------------------------------------------------------------------
+ * - The key point is to skip duplicate permutations due to the duplicate indexes
+ *   For numbers [1,2,2] the unique permutations are: 3!/2! = 3 unique permutations
+ *   
+ * - How to achieve that?
+ * - checking for nums[i] == nums[i-1] is not enough, 
+ * we also need to make sure if the index i or i-1  visited to skip adding that index to the tempList
+ * remember both visited[i-1] and !visited[i-1] is working to trace in the for loop skipping duplicates
  * main trick: if(visited[i] || i > 0 && nums[i] == nums[i-1] && visited[i-1] ) //gives ascending order
  * 			   if(visited[i] || i > 0 && nums[i] == nums[i-1] && !visited[i-1] ) //gives descending order
  *             Both are working the difference is while ordering, ascending or descending order
- *             
+ *  --------------------------------------------------------------------------------------------------------------
+ *  Time Complexity is obviously factorial time, because we can have total factorial permutations
+ *  Space Complexity, if you are not storing the output will be same as total number of characters in the input string = O(N!)
+ *  because we are going at max 4 level deep into recursion, which is the total depth of the input = O(N)
+ *            
  * https://www.youtube.com/watch?v=nYFd7VHKyWQ&list=PLrmLmBdmIlpslxZUHHWmfOzNn6cA7jvyh - recursion tree explanation
  * https://github.com/mission-peace/interview/blob/master/src/com/interview/recursion/StringPermutation.java					
  * */
@@ -58,12 +70,12 @@ public class Permutations2 {
 	}
 	
 	private void dfs(List<List<Integer>>result, ArrayList<Integer>tempList,int[] nums, boolean[] visited) {
-		if(tempList.size() == nums.length) {
+		if(tempList.size() == nums.length) { //once the tempList.size() equals to the given nums length means that we get one permutation
 			result.add(new ArrayList<Integer>(tempList));
 			return;
 		}
 		for(int i = 0; i < nums.length; i++) {
-			if(visited[i] || i > 0 && nums[i] == nums[i-1] && visited[i-1]) continue; //skip duplicate permutations
+			if(visited[i] || i > 0 && nums[i] == nums[i-1] && visited[i-1]) continue; //skip duplicate permutations; same with (visited[i] || i > 0 && nums[i] == nums[i-1] && !visited[i-1])
 			tempList.add(nums[i]);
 			visited[i] = true;
 			dfs(result, tempList, nums, visited);
--- Algorithms/main/java/ladders/GraphSearch/NQueens.java
+++ Algorithms/main/java/ladders/GraphSearch/NQueens.java
@@ -1,8 +1,13 @@
 package main.java.ladders.GraphSearch;
-
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
- * 33. N-Queens - Medium - Required
+ * 33. N-Queens - Medium - Required(LintCode)
+ * 51. N-Queens - Medium (LeetCode)
 
 The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
 
@@ -32,100 +37,91 @@ Challenge: Can you do it without recursion?
 
 Tags: Recursion Depth First Search
 
-Related Problems 
+Related Problems: 
 Medium Pacific Atlantic Water Flow 28 %
 Medium Combinations 33 %
 Medium N-Queens II 40 %
  * */
-/*Analysis: classic recursive problem
-* 1. Use a valid int vector to store the current state, A[i] = j refers that the ith row and
-* jth column is placed a queen
-* 2. Valid state: not tin the same column, which is A[i] != A[current], not in the
-* same diagonal direction: abs(A[i]-A[current]) != r-i
-* 3. Recursion: 
-* Start: placeQueen(0, n)
-* 	if (current == n then print result)
-* 	else
-* 		for each place less than n, 
-* 			place queen
-* 			if current state is valid, then place next queen placeQueen(cur+1, n)
-* 		end for 
-* 
+
+/*Analysis: Two queens cannot attack each-other only if both are not sharing the same row, same column, and same diagonal lane
+Use recursive DFS backtracking approach - Find out if the square is under attack or not
+------------------------------------------------------------------------------------------------
+For the N = 4 the board 4x4, but the index starts from 0
+if the queen is placed on (1, 2) then all the rows and columns shared with the queen is under attack
+
+row -> (1, 0), (1, 1), (1, 3)
+col -> (0, 2), (2, 2), (3, 2)
+
+How to find Diagonal attacks? 
+From top left to bottom right: (row - col) -> 1-2 = -1 -> (0, 1), (2, 3)
+From top right to bottom left: (row + col) -> 1+2 = 3 -> (3, 0), (2, 1), (0, 3)
+
+So, these are the squares that the queen placed on (1,2) can attack.
+--------------------------------------------------------------------------------------------------
+Step 1:  Since the board is NxN the recursion will be N level deep.(In the example it's 4x4 board, 4 level deep recursion)
+Place the 1st queen on the (0,0) increment the row and see what is the fist point we can place the 2nd queen,
+which the 1st queen cannot attack
+--------------------------------------------------------------------------------------------------
+Step 2: increment the row and process the same. If you reach at the end of the column, 
+then backtrack to the previous row again and see where else we can place the second queen
+---------------------------------------------------------------------------------------------------
+Step 4: Final result will be the placing all the N number of queens till reach the nxn board 
+--------------------------------------------------------------------------------------------------
+Time Complexity is Exponential(worst case) - because of backtracking and going back and forth
+Space Complexity is O(n) - recursion in the worst case will be N level deep for an NxN board
+--------------------------------------------------------------------------------------------------
+References:
+https://www.youtube.com/watch?v=xouin83ebxE&list=PLrmLmBdmIlpslxZUHHWmfOzNn6cA7jvyh&index=2
+https://github.com/mission-peace/interview/blob/master/src/com/interview/recursion/NQueenProblem.java
+
 *
 */
+
+//TODO However, I suggest that let’s use 3 boolean arrays instead. Let’s compare the run time:
+//HashSet: 15ms, beats 17.85%
+//Boolean Array: 5ms, beats 91.44%
+
 public class NQueens {
-	/**
+	 /**
      * Get all distinct N-Queen solutions
      * @param n: The number of queens
      * @return: All distinct solutions
      * For example, A string '...Q' shows a queen on forth position
      */
-	// Back-traacking approach
-	// wrapper method
-	ArrayList<ArrayList<String>> solveNQueens(int n){
-		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
-		if (n <= 0) {
-			return result;
-		}
-		search(n, new ArrayList<Integer>(), result);
-		return result;
-	}
+	//Recursive Backtracking
+	private Set<Integer> col = new HashSet<Integer>();
+	private Set<Integer> diag1 = new HashSet<Integer>();
+	private Set<Integer> diag2 = new HashSet<Integer>();
 	
-	/*
-	 * n : total rows / columns
-	 * cols : <row : queen column position>
-	 * result : <row : placement string>
-	 * */
-	private void search(int n, ArrayList<Integer> cols, ArrayList<ArrayList<String>> result) {
-		if (cols.size() == n) {
-			result.add(drawChessBoard(cols));
-			return;
-		}
-		
-		for (int col = 0; col < n; col++) {
-			if (!isValid(cols, col)) {
-				continue;
-			}
-			cols.add(col);
-			search(n, cols, result);
-			cols.remove(cols.size() - 1);
-		}
-	}
-	
-	private boolean isValid(ArrayList<Integer> cols, int col) {
-		int row = cols.size();
-		for (int i = 0; i < row; i++) {
-			// same column
-			if (cols.get(i) == col) {
-				return false;
-			}
-			// left-top to the right-bottom
-			if (i - cols.get(i) == row - col){
-				return false;
-			}
-			
-			// right-top to left bottom
-			if(i + cols.get(i) == row + col){
-				return false;
-			}
-		}
-		
-		return true;
-	}
-	
-	private ArrayList<String> drawChessBoard(ArrayList<Integer> cols) {
-		ArrayList<String> chessboard = new ArrayList<String>();
-		for (int i = 0; i < cols.size(); i++){
-			chessboard.add(i, "");
-			for (int j = 0; j < cols.size(); j++){
-				if (j == cols.get(i)){
-					chessboard.set(i, chessboard.get(i) + "Q");
-				} else {
-					chessboard.set(i, chessboard.get(i) + ".");
-				}
-			}
-		}
-		return chessboard;
-	}
-	
+    public List<List<String>> solveNQueens(int n) {
+    	List<List<String>> result = new ArrayList<List<String>>();
+    	dfs(result, new ArrayList<String>(), 0, n);
+    	return result;
+    }
+    
+    private void dfs(List<List<String>>result, List<String> tempList, int row, int n) {
+    	if(row == n) {
+    		result.add(new ArrayList<String>(tempList));
+    		return;
+    	}
+    	for(int i = 0; i < n; i++) {
+    		if(col.contains(i) || diag1.contains(row+i) || diag2.contains(row - i)) continue;
+    		char[] charArray = new char[n];
+    		Arrays.fill(charArray, '.');
+    		charArray[i] = 'Q';
+    		String rowString = new String(charArray);
+    		
+    		tempList.add(rowString);
+    		col.add(i);
+    		diag1.add(row+i);
+    		diag2.add(row - i);
+    		
+    		dfs(result, tempList, row + 1, n);
+    		tempList.remove(tempList.size() -1);
+    		col.remove(i);
+    		diag1.remove(row + i);
+    		diag2.remove(row - i);
+    	}
+    }
+
 }
