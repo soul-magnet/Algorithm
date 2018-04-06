2dda6b3212e49664766414d21576929de7ef0c36
soul-magnet <topkaya.emineirmak@gmail.com>
2018-04-06 09:53:51 -0400

    Regular Expression Matching - Backtracking - DP

--- Algorithms/main/java/ladders/DynamicProgramming/RegularExpressionMatching.java
+++ Algorithms/main/java/ladders/DynamicProgramming/RegularExpressionMatching.java
@@ -13,14 +13,18 @@ The matching should cover the entire input string (not partial).
 The function prototype should be:
 bool isMatch(string s, string p)
  
-Example
-isMatch("aa","a") → false
-isMatch("aa","aa") → true
-isMatch("aaa","aa") → false
-isMatch("aa", "a*") → true
-isMatch("aa", ".*") → true
-isMatch("ab", ".*") → true
-isMatch("aab", "c*a*b") → true
+Some examples: 
+isMatch("aa","a") → false 
+isMatch("aa","aa") → true 
+isMatch("aaa","aa") → false 
+isMatch("aa", "a") → true 
+isMatch("aa", ".") → true 
+isMatch("ab", ".") → true 
+isMatch("aab", "ca*b") → true
+
+Make sure we totally understand the question.
+“.” is easy, just representing a single char.
+“x*” represent 0 or more x, e.g., “”,”x”,”xx”,”xxx”,”xxxxxx”…
 
 Tags: String Backtracking Dynamic Programming Facebook Google
 
@@ -36,7 +40,44 @@ public class RegularExpressionMatching {
      */
 	
 	//Version 1: Recursive - backtracking
-	public boolean isMatch(String s, String p) {
+	//Reduction: 
+	//1. If next char is not "*", we jsut compare this current char, and reduce both the length of s and p by 1.
+	//2. If next char is "*":
+	//	2.1 If the current char is the same, reduces s by 1 and check again.Until the current char is different -Not Accurate
+	//      Consider the case "aaa" and "a*a." So every time before we reduce p by 1, we have to check if they match.
+	//		This check must to be placed before we reduce p by 1, since we could have the case: s=abc p=.*abc
+	//	2.2 If the current char is different reduce p by 2.
+	//3. Base Case: As reduction reduces the length by 1 or 2 every time, we need to consider finally when length == 0 or length == 1
+	//	3.1 if p.length() == 0, check if s.length() is also 0.
+	//	3.2 If p.length() == 1, check if s.length() is also 1 and the match or not
+	//4. A detail; s.subString(l), could return null if the l is the length of the s.
+	//5. A detail: always check s.length
+	public boolean isMatch1(String s, String p) {
+        if (p.length() == 0){
+            return s.length() == 0;
+        }
+        if (p.length() == 1){
+            return s.length() == 1 && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.');
+        }
+         
+        // Now, we make sure that p has at least 2 chars.
+        if (p.charAt(1) != '*'){
+            if (s.length() == 0) return false;
+            else return  (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.')&& isMatch(s.substring(1), p.substring(1));
+ 
+        }else{ // p.charAt(1) == '*''
+            while (s.length() > 0 && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.')){
+                if (isMatch(s,p.substring(2))) return true;  // this has to be before s= s.substring(1), to deal with the case: s=abc  p = .*abc
+                 
+                s = s.substring(1);
+            }
+            return isMatch(s, p.substring(2));
+        }
+         
+    }
+	
+	//Version 2: DP 
+	public boolean isMatch2(String s, String p) {
 		return dp(0, s, 0, p);
 	}
 	
@@ -48,7 +89,7 @@ public class RegularExpressionMatching {
 			while(i < sn && (p.charAt(j) == s.charAt(i) || p.charAt(j) == '.')) if(dp(++i, s, j+2, p)) return true;
 		}else if(i<sn && (p.charAt(j) == '.' || s.charAt(i) == p.charAt(j)) && dp(i+1, s, j+1, p)) return true;
 		
-		return 
+		return false; 
 	}
     public boolean isMatch(String s, String p) {
         // base case
